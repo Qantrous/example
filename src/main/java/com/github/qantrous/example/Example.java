d990def0980e8b182c7a3c945401d76959755fd0
Qantrous <ikant14@freeuni.edu.ge>
2019-06-15 00:17:59 +0400

    first commit

---
+++ src/main/java/com/github/qantrous/example/Example.java
@@ -0,0 +1,13 @@
+package com.github.qantrous.example;
+
+public class Example {
+
+    public static Long add(Long a, Long b) {
+        return a + b;
+    }
+
+    public static Long substract(Long a, Long b) {
+        return a-b;
+    }
+
+}
